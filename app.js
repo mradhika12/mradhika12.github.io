643cb35aaba30737c6a99e65453a86cf0340d5a5
mradhika12 <rathi2000ka@gmail.com>
2020-10-22 10:01:28 +0530

    add new file

---
+++ app.js
@@ -0,0 +1,67 @@
+//var x=10;
+//var y=20;
+//var a;
+//a=x
+//x=y
+//y=a
+//console.log("x="+x ,"y="+y)
+/*var a=[2,5,4,8]
+a.push(5);
+console.log(a);
+a.pop();
+console.log(a);
+a.unshift(9);
+console.log(a);
+a.shift();
+console.log(a[0]);*/
+/*var a=[3,5,2,7,9,1,]
+var i;
+for(i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}*/
+/*var a=["23","45","34","44"]
+var v=a.valueOf();
+console.log(v);
+*/
+//swapping two integers
+var x=10;
+var y=20;
+var a;
+a=x;
+x=y;
+y=a;
+console.log("x="+x,"y="+y);
+
+//Add a value at last item
+var a=[2,99,45,23];
+a.push(5,4);
+for(i=0;i<a.length;i++){
+    console.log(a[i]);
+}
+
+//Remove the last item
+var a=[2,4,6,1];
+a.pop();
+for(i=0;i<a.length;i++){
+    console.log(a[i]);
+}
+
+//Add a value at first item
+var a=[3,5,8,9];
+a.unshift(3);
+for(i=0;i<a.length;i++){
+    console.log(a[i]);
+}
+
+//Remove the first item
+var a=[5,7,9,2];
+a.shift();
+for(i=0;i<a.length;i++){
+    console.log(a[i]);
+}
+ 
+
+
+
+
